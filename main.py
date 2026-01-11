52f340beccf05f789fa26241dd854862bb97b430
Dupye <163210565+Dupye@users.noreply.github.com>
2026-01-10 22:53:17 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# app.py
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Rota /verify_udid
+@app.route('/verify_udid', methods=['POST'])
+def verify_udid():
+    return jsonify({
+        "status": "success",
+        "message": "UDID verificado com sucesso!"
+    })
+
+# Rota /active_udid
+@app.route('/active_udid', methods=['POST'])
+def active_udid():
+    return jsonify({
+        "status": "success",
+        "message": "UDID ativado com sucesso!"
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
